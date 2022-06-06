9986eedf20cf699e643df816699d2b3bc01dc3d8
nagarajnsg1995 <nagarajaece.rymec@gmail.com>
2022-06-06 21:00:36 +0530

    added

--- src/EWage.java
+++ src/EWage.java
@@ -1,8 +1,20 @@
 public class EWage {
+
+
     public static void main(String[] args) {
+        int ispresent = 1;
         System.out.println("wellcome to employee wage");
+
+        double empcheck = Math.floor(Math.random() * 10) % 2;
+        if (empcheck == ispresent)
+            System.out.println("emp is present");
+        else
+            System.out.println("emp is abscent");
+
     }
 }
+
+
 
 
 
